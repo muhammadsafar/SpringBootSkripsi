4d69947323a13e7706a28d489e0a27808f00cda7
Muhammad Safar Abdurrahman <muhammad.ict1487@gmail.com>
2018-09-17 15:27:56 +0700

    commit

---
+++ src/main/java/com/skripsi/SpringBootSkripsi/SpringBootSkripsiApplication.java
@@ -0,0 +1,12 @@
+package com.skripsi.SpringBootSkripsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSkripsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSkripsiApplication.class, args);
+	}
+}
